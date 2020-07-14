174f34122a372d1708327b6e38874b8b448428c3
Simon Buist <simon.buist@altran.com>
2020-07-14 13:40:10 +0100

    Update mutt config

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 eval $(ssh-agent)
 ssh-add
-tmux new -d 'mutt' \; \
+tmux new -d 'neomutt' \; \
   new-window -d 'cd ~/spark2014; zsh -i' \; \
   new-window -d 'vim /home/simon/spark2014/src/flow/flow_generated_globals-phase_2.adb +864' \; \
   new-window -d 'cd ~/spark2014/testsuite/gnatprove/tests/T213-023__flow_unchecked; zsh -i' \; \
