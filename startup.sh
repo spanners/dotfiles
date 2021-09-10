6b6e3ce644b046ac2951a1c31d3e31781d3ecc93
Simon Buist <simon.buist@altran.com>
2021-09-10 11:00:47 +0100

    Update startup script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 eval $(ssh-agent)
-ssh-add
+ssh-add ~/.ssh/id_rsa_adacore
+ssh-add ~/.ssh/id_rsa_spanners
 tmux new -n 'make' -d 'cd ~/spark2014; zsh -i' \; \
   new-window -n 'test' -d 'cd /home/simon/spark2014/testsuite/gnatprove/; zsh -i' \; \
   new-window -n 'U824-022' -d 'cd /home/simon/spark2014/testsuite/gnatprove/tests/U824-022__flow_detect_loop_exit; zsh -i' \; \
